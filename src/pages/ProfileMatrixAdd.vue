d9738e8f03151751570c9a05050c85e9e6d94b31
Kenost <Kenostru@mail.ru>
2024-04-27 14:08:54 +0500

    matrix

--- src/pages/ProfileMatrixAdd.vue
+++ src/pages/ProfileMatrixAdd.vue
@@ -22,107 +22,94 @@
       </div>
       <div class="dart-form-group picker-wrap">
         <label for="name">Добавление товаров</label>
-        <!-- <PickList v-model="available_products.products" :selection="form.products" listStyle="height:342px" dataKey='id' :showSourceControls="false" :showTargetControls="false"
-        @update:selection="saveData(data)">
-          <template #sourceheader>
-            <div class="source_list">
-              <span>Доступные</span>
-              <div class="filter">
-                <div class="form_input_group input_pl input-parent required">
-                  <input
-                  type="text"
-                  id="filter_name"
-                  placeholder="Артикул, наименование"
-                  name="filter"
-                  class="dart-form-control"
-                  v-model="filter"
-                  @input="setFilter"
-                  />
-                  <label for="product_filter_name" class="s-complex-input__label">Артикул, наименование</label>
-                  <div class="form_input_group__icon">
-                      <i class="d_icon d_icon-search"></i>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </template>
-          <template #targetheader> Выбранные </template>
-          <template #item="slotProps">
-              <div class="flex flex-wrap p-2 align-items-center gap-3">
-                  <img class="w-4rem flex-shrink-0 border-round" :src="'https://mst.tools' + slotProps.item.image" :alt="slotProps.pagetitle" />
-                  <div class="flex-1 flex flex-column gap-2">
-                      <span class="font-bold">{{ slotProps.item.name }}</span>
-                      <span class="article">{{ slotProps.item.article }}</span>
-                  </div>
-              </div>
-          </template>
-        </PickList> -->
 
         <div class="PickList">
           <div class="PickList__product">
             <b class="PickList__title">Доступные товары</b>
+            <div class="PickList__filters">
+              <div class="form_input_group input_pl input-parent required">
+                <input
+                type="text"
+                id="filter_name"
+                placeholder="Введите артикул или название"
+                class="dart-form-control"
+                v-model="filter.name"
+                @input="setFilter('filter')"
+                />
+                <label for="product_filter_name" class="s-complex-input__label">Введите артикул или название</label>
+                <div class="form_input_group__icon">
+                    <i class="d_icon d_icon-search"></i>
+                </div>
+              </div>
+              <div class="dart-form-group">
+                <TreeSelect v-model="this.filter.category" :options="this.get_catalog" selectionMode="checkbox" placeholder="Выберите категорию" class="w-full" @change="setFilter"/>
+              </div>
+            </div>
             <div class="PickList__products">
-              <div class="PickList__el" v-for="item in this.available_products?.products" :key="item.id">
+              <div class="PickList__el" v-for="item in this.products" :key="item.id">
                 <img :src="'https://mst.tools' + item.image" alt="">
                 <div class="PickList__product-info">
                   <div class="PickList__name">{{item.name}}</div>
                   <div class="PickList__article">{{item.article}}</div>
+                  <div class="PickList__price">{{Number(item.price).toFixed(0)}} ₽</div>
                 </div>
-                <div class="PickList__select"><i class="pi pi-angle-right"></i></div>
+                <div @click="select(item.id)" class="PickList__select"><i class="pi pi-angle-right"></i></div>
               </div>
             </div>
           </div>
 
           <div class="PickList__selected">
-            <b class="PickList__title">Доступные товары</b>
-            <div class="PickList__products">
-              <div class="PickList__el" v-for="item in this.available_products?.products" :key="item.id">
-                <img :src="'https://mst.tools' + item.image" alt="">
-                <div class="PickList__product-info">
-                  <div class="PickList__name">{{item.name}}</div>
-                  <div class="PickList__article">{{item.article}}</div>
-                </div>
-                <div class="PickList__select"><i class="pi pi-times"></i></div>
+            <div class="PickList__title mb-4">
+              <b>Добавленные товары</b>
+              <div class="PickList__title-left">
+                <b>Скидка в %</b>
+                <b>Скидка в ₽</b>
+                <b>Итоговая цена в ₽</b>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
-      <div class="dart-form-group picker-wrap">
-        <label for="name">Настройка номенклатуры</label>
-        <div class="product-settings" v-for="(item, index) in selected" :key="index">
-          <div class="item">
-            <div class="dart-row dart-align-items-center">
-              <div class="d-col-md-4">
-                <div class="flex flex-wrap p-2 align-items-center gap-3">
-                  <img class="w-4rem flex-shrink-0 border-round" :src="'https://mst.tools' + item.image" :alt="item.pagetitle" />
-                  <div class="flex-1 flex flex-column gap-2">
-                      <span class="font-bold">{{ item.name }}</span>
-                      <span class="article">{{ item.article }}</span>
+            <div class="PickList__products">
+              <div class="PickList__el" v-for="(item, index) in this.selected" :key="item.id">
+                <img :src="'https://mst.tools' + item.image" alt="">
+                  <div class="PickList__info">
+                  <div class="PickList__product-info off">
+                    <div class="PickList__name">{{item.name}}</div>
+                    <div class="PickList__article">{{item.article}}</div>
+                    <div class="PickList__price">{{Number(item.price).toFixed(0)}} ₽</div>
+                  </div>
+                  <div class="PickList__values">
+                    <InputNumber
+                      v-model="this.selected[index].discount_proccent"
+                      inputId="horizontal-buttons"
+                      :step="1"
+                      min="0"
+                      max="100"
+                      suffix=" %"
+                      @update:modelValue="setPrices(index, 'discount_proccent', this.selected[index].discount_proccent)"
+                      incrementButtonIcon="pi pi-plus" decrementButtonIcon="pi pi-minus"
+                    />
+                    <InputNumber
+                      v-model="selected[index].discount_money"
+                      inputId="horizontal-buttons"
+                      :step="1"
+                      min="0"
+                      :max="item.price"
+                      mode="currency" currency="RUB"
+                      @input="setPrices(index, 'discount_money', this.selected[index].discount_money)"
+                      incrementButtonIcon="pi pi-plus" decrementButtonIcon="pi pi-minus"
+                    />
+                    <InputNumber
+                      v-model="selected[index].final_price"
+                      inputId="horizontal-buttons"
+                      :step="1"
+                      :max="item.price"
+                      mode="currency" currency="RUB"
+                      min="0"
+                      @input="setPrices(index, 'final_price', this.selected[index].final_price)"
+                      incrementButtonIcon="pi pi-plus" decrementButtonIcon="pi pi-minus"
+                    />
                   </div>
                 </div>
-              </div>
-              <div class="d-col-md-4">
-                <span class="p-float-label">
-                  <InputNumber
-                    v-model="selected[index].count"
-                    inputId="horizontal-buttons"
-                    :step="1"
-                    incrementButtonIcon="pi pi-plus" decrementButtonIcon="pi pi-minus"
-                  />
-                  <label for="number-input">Кол-во товара</label>
-                </span>
-              </div>
-              <div class="d-col-md-4">
-                <span class="p-float-label">
-                  <InputNumber
-                    v-model="selected[index].days"
-                    inputId="horizontal-buttons"
-                    :step="1"
-                    incrementButtonIcon="pi pi-plus" decrementButtonIcon="pi pi-minus"
-                  />
-                  <label for="number-input">Разрешенное кол-во дней</label>
-                </span>
+                <div @click="deleteSelect(item.id)" class="PickList__select"><i class="pi pi-times"></i></div>
               </div>
             </div>
           </div>
@@ -137,6 +124,7 @@ import { mapActions, mapGetters } from 'vuex'
 import router from '@/router'
 import Calendar from 'primevue/calendar'
 import InputNumber from 'primevue/inputnumber'
+import TreeSelect from 'primevue/treeselect'
 
 export default {
   name: 'ProfileMatrixAdd',
@@ -144,17 +132,45 @@ export default {
   data () {
     return {
       loading: false,
-      filter: '',
+      filter: {
+        name: '',
+        category: {}
+      },
       selected: [],
+      products: [],
       form: {
-      }
+      },
+      get_catalog: []
     }
   },
   methods: {
     ...mapActions([
       'get_available_products_from_api',
-      'set_matrix_to_api'
+      'set_matrix_to_api',
+      'get_catalog_from_api'
     ]),
+    select (id) {
+      const product = this.products.find(r => r.id === id)
+      this.selected.push(product)
+      this.products = this.products.filter((r) => r.id !== id)
+    },
+    setPrices (index, name, value) {
+      switch (name) {
+        case 'discount_proccent':
+          this.selected[index].discount_money = (Number(this.selected[index].price) / 100) * value
+          this.selected[index].final_price = Number(this.selected[index].price) - this.selected[index].discount_money
+          break
+        case 'discount_money':
+          break
+        case 'final_price':
+          break
+      }
+    },
+    deleteSelect (id) {
+      const product = this.selected.find(r => r.id === id)
+      this.products.push(product)
+      this.selected = this.selected.filter((r) => r.id !== id)
+    },
     setFilter () {
       const data = { filter: this.filter, selected: this.selected }
       this.get_available_products_from_api(data)
@@ -184,13 +200,27 @@ export default {
     }
   },
   mounted () {
-    this.get_available_products_from_api({ filter: '', selected: [] })
+    this.get_available_products_from_api({ filter: '', selected: [] }).then(
+      this.products = this.available_products.products
+    )
+    this.get_catalog_from_api().then(
+      this.get_catalog = this.getcatalog
+    )
   },
-  components: { Calendar, InputNumber },
+  components: { Calendar, TreeSelect, InputNumber },
   computed: {
     ...mapGetters([
-      'available_products'
+      'available_products',
+      'getcatalog'
     ])
+  },
+  watch: {
+    getcatalog: function (newVal, oldVal) {
+      this.get_catalog = newVal
+    },
+    available_products: function (newVal, oldVal) {
+      this.products = newVal.products
+    }
   }
 }
 </script>
@@ -244,15 +274,18 @@ export default {
     width: 100%;
     display: flex;
     justify-content: space-between;
+    gap: 40px;
 
     &__title{
       width: 100%;
       display: flex;
-      padding: 24px;
+      padding: 16px 24px;
       border-bottom: 1px solid rgba(0, 0, 0, 0.12);
       font-size: 14px;
       font-weight: 500;
       line-height: 15px;
+      justify-content: space-between;
+      align-items: center;
     }
 
     &__product{
@@ -262,7 +295,7 @@ export default {
     }
 
     &__selected{
-      width: calc(60% - 100px);
+      width: 60%;
       border-radius: 5px;
       border: 1px solid rgba(0, 0, 0, 0.12);
     }
@@ -273,7 +306,7 @@ export default {
       display: flex;
       flex-direction: column;
       gap: 24px;
-      padding: 24px;
+      padding: 0 24px 24px 24px;
     }
 
     &__el{
@@ -287,6 +320,21 @@ export default {
 
     &__product-info{
       width: calc(100% - 85px);
+
+      &.off{
+        width: auto;
+      }
+    }
+
+    &__title-left{
+      display: flex;
+      align-items: center;
+      margin-right: 54px;
+      gap: 16px;
+      b{
+        width: 90px;
+        text-align: center;
+      }
     }
 
     &__name{
@@ -303,6 +351,22 @@ export default {
       line-height: 16px;
     }
 
+    &__info{
+      width: calc(100% - calc(85px + 54px));
+      margin-right: 10px;
+      display: flex;
+      align-items: center;
+      align-items: flex-start;
+    }
+
+    &__values{
+      display: flex;
+      gap: 16px;
+      .p-inputnumber{
+        width: 90px;
+      }
+    }
+
     &__select{
       border-radius: 5px;
       width: 44px;
@@ -313,6 +377,35 @@ export default {
       justify-content: center;
       color: #FFF;
       cursor: pointer;
+    }
+
+    &__price{
+      font-size: 14px;
+      font-weight: 500;
+      color: #282828;
+      line-height: 16px;
+      margin-top: 8px;
+    }
+
+    &__filters{
+      padding: 16px 24px;
+      display: flex;
+      align-items: center;
+      gap: 8px;
+
+      .dart-form-group{
+        width: 50%;
+      }
+
+      .form_input_group {
+        width: 50%;
+      }
+
+      .form_input_group__icon{
+        padding: 0;
+        top: 55%;
+        right: 10px;
+      }
     }
   }
 </style>
