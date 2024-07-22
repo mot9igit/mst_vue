e820af21e9d042798fab512f4742f73a85048b1b
Kenost <Kenostru@mail.ru>
2024-07-22 13:39:59 +0500

    fix

--- src/components/opt/TableCatalogRow.vue
+++ src/components/opt/TableCatalogRow.vue
@@ -34,7 +34,7 @@
       </td>
       <td>{{item.store_name}}</td>
       <td>{{Math.round(item.price).toLocaleString('ru')}} ₽</td>
-      <td>{{item.old_price ? Math.round(item.old_price).toLocaleString('ru') : Math.round(item.price).toLocaleString('ru')}} ₽ / {{item.actions.length == 0 ? 0 : item.old_price - item.price}} ₽</td>
+      <td>{{item.old_price ? Math.round(item.old_price).toLocaleString('ru') : Math.round(item.price).toLocaleString('ru')}} ₽ / {{item.actions.length == 0 ? 0 : ((item.old_price - item.price).toFixed(0)).toLocaleString('ru')}} ₽</td>
       <td>{{item.delay ? Number(item.delay).toFixed(1) + ' дн' : 'Нет'}}</td>
       <td>{{item.payer === '1' ? 'Поставщик' : 'Покупатель'}} </td>
       <td>от {{item.delivery}} дн ({{new Date(item.delivery_day).toLocaleString("ru", {month: 'long', day: 'numeric'})}})</td>
